613ea89311589c5d0cfdd15af819344a266d2519
Dave Augustus <davetweetlive@gmail.com>
2020-02-05 18:58:57 +0530

    Basic configurations

---
+++ source_file/content.js
@@ -0,0 +1,10 @@
+console.log("Saffron Coders!")
+
+let paragraphs = document.getElementsByTagName("p")
+for(elt of paragraphs){
+    elt.style['background-color'] = "pink";
+}
+
+
+let videoFiles = document.getElementsByTagName('video');
+console.log(videoFiles);
